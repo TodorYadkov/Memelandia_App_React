d2a28ac3315f5fe9e8fb23c78cd1ac7f2bcf3132
Todor Yadkov <toni_12@abv.bg>
2023-10-28 10:07:39 +0300

    Create routesConfig.js

---
+++ server/config/routesConfig.js
@@ -0,0 +1,22 @@
+const displayReq = require('../middlewares/displayReq');
+const userController = require('../controllers/userController');
+
+module.exports = (app) => {
+    // Display each request
+    app.use(displayReq());
+
+    // Main routes
+    app.use('/users/', userController)
+
+    // Use 404 no content
+    app.all('*', (req, res, next) => {
+        try {
+            throw new Error(`No content - path ${req.path} of method ${req.method} not found`);
+
+        } catch (error) {
+
+            error.statusCode = 404;
+            next(error);
+        }
+    });
+};
