3dc08589db8b2a1f18cf4dcc4ed6baa16794632c
Todor Yadkov <toni_12@abv.bg>
2023-10-28 19:44:57 +0300

    Fix problems, add forgotten password functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { userRegister, userLogin, userLogout, getUserById, updateUserById } = require('../services/userService');
-const { onlyForGuest, isAuth } = require('../middlewares/guards');
+const { userRegister, userLogin, userLogout, getUserById, updateUserById, forgottenPassword } = require('../services/userService');
+const { isGuest, isAuth } = require('../middlewares/guards');
 const { validateUserRegistrationSchema, validateUserLoginSchema, validateUserUpdateUserSchema } = require('../util/validationSchemes');
 
 // User register - Not logged in
-router.post('/register', onlyForGuest, async (req, res, next) => {
+router.post('/register', isGuest, async (req, res, next) => {
     try {
         // Validate user input
         await validateUserRegistrationSchema.validateAsync(req.body);
@@ -19,7 +19,7 @@ router.post('/register', onlyForGuest, async (req, res, next) => {
 });
 
 // User login - Not logged in
-router.post('/login', onlyForGuest, async (req, res, next) => {
+router.post('/login', isGuest, async (req, res, next) => {
     try {
         // Validate user input
         await validateUserLoginSchema.validateAsync(req.body);
@@ -36,6 +36,7 @@ router.post('/login', onlyForGuest, async (req, res, next) => {
 // User logout - Logged in
 router.get('/logout', isAuth, async (req, res, next) => {
     try {
+
         const userToken = req.userToken;
         await userLogout(userToken)
 
@@ -45,10 +46,10 @@ router.get('/logout', isAuth, async (req, res, next) => {
     }
 });
 
-// Get user by id - Logged in
-router.get('/profile/:userId', isAuth, async (req, res, next) => {
+// Get user by Id - Logged in
+router.get('/profile', isAuth, async (req, res, next) => {
     try {
-        const userId = req.params.userId;
+        const userId = req.user._id;
         const user = await getUserById(userId);
 
         res.status(200).json(user);
@@ -58,17 +59,32 @@ router.get('/profile/:userId', isAuth, async (req, res, next) => {
 });
 
 // Update user by id - Logged in
-router.put('/profile/edit/:userId', isAuth, async (req, res, next) => {
+router.put('/profile', isAuth, async (req, res, next) => {
     try {
-
         // Validate user input
         await validateUserUpdateUserSchema.validateAsync(req.body);
 
         const userData = req.body;
-        const userId = req.params.userId;
+        const userId = req.user._id;
         const user = await updateUserById(userData, userId);
 
         res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Forgotten password
+// TODO: This could be optimized with a return email confirmation service, now is only for school purpose
+router.put('/forgotten-password', isGuest, async (req, res, next) => {
+    try {
+        // Validate user input
+        await validateUserUpdateUserSchema.validateAsync(req.body);
+
+        const userData = req.body;
+        const result = await forgottenPassword(userData); // Result for this function is object with message for accept change password or accessToken
+
+        res.status(200).json(result);
     } catch (error) {
         next(error);
     }
