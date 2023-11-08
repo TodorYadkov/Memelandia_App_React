d45b0c24e5831628a2bcff9023b4ef69e9fae42b
Todor Yadkov <toni_12@abv.bg>
2023-11-08 08:39:14 +0200

    Create separate file with user field names

---
+++ client/src/components/features/users/userFieldConstants.jsx
@@ -0,0 +1,8 @@
+export const USER_FIELD = {
+    username: 'username',
+    email: 'email',
+    name: 'name',
+    age: 'age',
+    password: 'password',
+    securityQuestion: 'securityQuestion'
+};
