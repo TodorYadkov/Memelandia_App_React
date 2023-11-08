153564da43b7c380e3b84eea27d658b38582b06f
Todor Yadkov <toni_12@abv.bg>
2023-11-08 08:32:17 +0200

    Create session storage manager

---
+++ client/src/components/core/hooks/useSessionStorage.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import { tokenName } from '../environments/constants';
+
+export const useSessionStorage = () => {
+    // User data is saved in context to easy use in app: { _id, username, email, name, age, rating, createdUser, updatedUser }
+    const [currentUserData, setCurrentUserData] = useState({});
+    
+    // Initial user token is saved in local storage --> can be null or accessToken
+    const [currentUserToken, setCurrentUserToken] = useState(() => {
+        try {
+            // Verify that the data in the local storage is in the correct format
+            const localStorageData = JSON.parse(localStorage.getItem(tokenName));
+            if (localStorageData) {
+                return localStorageData;
+            }
+
+        } catch (error) {
+            // TODO: Here can add some console log to show if there is any error
+            return null;
+        }
+
+        return null;
+    });
+
+    // Set new user state
+    const setUserState = (userData) => {
+        // If has a userData set new user state and user data
+        if (userData) {
+            localStorage.setItem(tokenName, JSON.stringify(userData.accessToken));
+            setCurrentUserToken(userData.accessToken); // The token is used to make a request to the server
+            setCurrentUserData(userData.userDetails);  // User data is used to get needed properties in entire app using a context
+        }
+    };
+
+    // Clear current user state
+    const clearUserState = () => {
+        localStorage.removeItem(tokenName);
+        setCurrentUserToken(null); // Set new state to null
+        setCurrentUserData({});    // Set new state to empty object
+    };
+
+    return {
+        setUserState,
+        clearUserState,
+        currentUserToken,
+        currentUserData,
+    };
+};
