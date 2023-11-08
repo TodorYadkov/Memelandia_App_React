7c9da1c24103f25e54e74ecc950e983cc0556bf3
Todor Yadkov <toni_12@abv.bg>
2023-11-08 08:38:40 +0200

    Create Rating component

--- client/src/components/features/rating/Rating.jsx
+++ client/src/components/features/rating/Rating.jsx
@@ -1,13 +1,15 @@
 import styles from './Rating.module.css';
 
-export default function Rating() {
+// eslint-disable-next-line react/prop-types
+export default function Rating({ rating }) {
+
     return (
         <div className={styles['rating']}>
-            <span className={`${styles['rating-star']} ${styles['active']}`} data-rating="1"></span>
-            <span className={styles['rating-star']} data-rating="2"></span>
-            <span className={styles['rating-star']} data-rating="3"></span>
-            <span className={styles['rating-star']} data-rating="4"></span>
-            <span className={styles['rating-star']} data-rating="5"></span>
+            <span className={`${styles['rating-star']}${rating >= 1 ? ` ${styles['active']}` : ''}`}></span>
+            <span className={`${styles['rating-star']}${rating >= 2 ? ` ${styles['active']}` : ''}`}></span>
+            <span className={`${styles['rating-star']}${rating >= 3 ? ` ${styles['active']}` : ''}`}></span>
+            <span className={`${styles['rating-star']}${rating >= 4 ? ` ${styles['active']}` : ''}`}></span>
+            <span className={`${styles['rating-star']}${rating >= 5 ? ` ${styles['active']}` : ''}`}></span>
         </div>
     );
 }
