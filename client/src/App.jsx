f0ae8e28c7562192602abec379532889ccfc42f9
Todor Yadkov <toni_12@abv.bg>
2023-11-08 08:30:20 +0200

    Add new path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 
+import { AuthProvider } from './components/core/contexts/AuthContext';
+
 import About from './components/features/about/About';
 import Footer from './components/features/footer/Footer';
 import Header from './components/features/header/Header';
@@ -9,12 +11,14 @@ import ListMemes from './components/features/meme/list-memes/ListMemes';
 import Login from './components/features/users/login/Login';
 import Profile from './components/features/users/profile/Profile';
 import Register from './components/features/users/register/Register';
+import ListUserMemes from './components/features/meme/list-user-memes/ListUserMemes';
+import Logout from './components/features/users/logout/Logout';
 
 
 function App() {
 
     return (
-        <>
+        <AuthProvider>
             <Header />
             <main className="site-main container">
                 <div className="site-main-background-wrapper">
@@ -23,12 +27,15 @@ function App() {
 
                         <Route path='/login' element={<Login />} />
                         <Route path='/register' element={<Register />} />
+                        <Route path='/logout' element={<Logout />} />
                         <Route path='/profile' element={<Profile />} />
 
                         <Route path='/memes/' element={<Outlet />}>
                             <Route path='catalog' element={<ListMemes />} />
+                            <Route path='user-memes/:userId' element={<ListUserMemes />} />
                             <Route path='details/:memeId' element={<DetailsMeme />} />
-                            {/* <Route path='edit/:memeId' element={< />} />
+                            {/*<Route path='create' element={< />} />
+                            <Route path='edit/:memeId' element={< />} />
                             <Route path='delete/:memeId' element={< />} /> */}
                         </Route>
 
@@ -37,7 +44,7 @@ function App() {
                 </div >
             </main >
             <Footer />
-        </>
+        </AuthProvider>
     );
 }
 
