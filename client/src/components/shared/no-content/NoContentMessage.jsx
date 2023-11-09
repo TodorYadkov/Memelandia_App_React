efd2be9559b1a7591ceb9c45f5512b48acfc92d9
Todor Yadkov <toni_12@abv.bg>
2023-11-09 02:19:11 +0200

    Replace anchor tag with Link

--- client/src/components/shared/no-content/NoContentMessage.jsx
+++ client/src/components/shared/no-content/NoContentMessage.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
+
 import styles from './NoContentMessage.module.css';
 
 export default function NoContentMessage() {
     return (
-        <h3 className={styles['no-content']}>Add your first Meme now from<a href="#"> here <i className="fa-regular fa-face-grin"></i></a></h3>
+        <h3 className={styles['no-content']}>Add your first Meme now from<Link to="/memes/create"> here <i className="fa-regular fa-face-grin"></i></Link></h3>
     );
 }
