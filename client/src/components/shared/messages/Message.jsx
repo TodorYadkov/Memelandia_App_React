6607fe274f336794f178d6e18e1e9f4ff856ab4d
Todor Yadkov <toni_12@abv.bg>
2023-11-09 02:18:29 +0200

    Refactoring

--- client/src/components/shared/messages/Message.jsx
+++ client/src/components/shared/messages/Message.jsx
@@ -1,76 +1,22 @@
-import { useEffect, useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import styles from './Message.module.css';
 
-// eslint-disable-next-line react/prop-types
+// Possible type: simple, success, error, warning
 export default function Message({ type, message }) {
-    const [isShownMessage, setIsShowMessage] = useState(true);
+    const [isShowMessage, setIsShowMessage] = useState(true);
 
-    useEffect(() => {
-        return () => {
-            setIsShowMessage(true); // Reset the state when the component unmount
-        };
-    }, []);
-
-    const closeHandler = (e) => {
+    const closeHandler = () => {
         // Hide message
-        setIsShowMessage(!isShownMessage);
+        setIsShowMessage(false);
     };
 
-    if (type === 'simple') {
+    if (isShowMessage) {
         return (
-            <>
-                {
-                    isShownMessage && (
-                        <div className={`${styles['alert']} ${styles['simple-alert']} ${styles['clear-error']}`}>
-                            <h6>{message}</h6>
-                            <p onClick={closeHandler} className={styles['close']}><span>&times;</span></p>
-                        </div>
-                    )
-                }
-            </>
-
-        );
-
-    } else if (type === 'success') {
-        return (
-            <>
-                {
-                    isShownMessage && (
-                        <div className={`${styles['alert']} ${styles['success-alert']}  ${styles['clear-error']}`}>
-                            <h6>{message}</h6>
-                            <p onClick={closeHandler} className={styles['close']}><span>&times;</span></p>
-                        </div>
-                    )
-                }
-            </>
-
-        );
-    } else if (type === 'error') {
-        return (
-            <>
-                {
-                    isShownMessage && (
-                        <div className={`${styles['alert']} ${styles['danger-alert']}  ${styles['clear-error']}`}>
-                            <h6>{message}</h6>
-                            <p onClick={closeHandler} className={styles['close']}><span>&times;</span></p>
-                        </div>
-                    )
-                }
-            </>
-
-        );
-    } else if (type === 'warning') {
-        return (
-            <>
-                {
-                    isShownMessage && (
-                        <div className={`${styles['alert']} ${styles['warning-alert']}  ${styles['clear-error']}`}>
-                            <h6>{message}</h6>
-                            <p onClick={closeHandler} className={styles['close']}><span>&times;</span></p>
-                        </div>
-                    )
-                }
-            </>
+            <div className={`${styles['alert']} ${styles[`${type}-alert`]} ${styles['clear-error']}`}>
+                <h6>{message}</h6>
+                <p onClick={closeHandler} className={styles['close']}><span>&times;</span></p>
+            </div>
         );
     }
 
