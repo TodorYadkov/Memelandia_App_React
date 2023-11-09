298b7102aef4d98ec1700de3645ce875ecc06779
Todor Yadkov <toni_12@abv.bg>
2023-11-09 02:10:35 +0200

    Add new page 404

---
+++ client/src/components/features/not-found-404/NotFound404.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+import styles from './NotFound404.module.css';
+
+export default function NotFound404() {
+    return (
+        <div className={`${styles['not-found']} max-width`}>
+            <div className={styles['not-found-404']}>
+                <h1 className={styles['not-found-heading']}>
+                    <span>4</span>
+                    <span className={styles['not-found-emoji']}>
+                        <img src="/assets/emoji-crying.png" alt="Emoji crying" />
+                    </span>
+                    <span>4</span>
+                </h1>
+            </div>
+            <h2>Oops! Page Not Be Found</h2>
+            <p>Sorry, but the page you are looking for does not exist, has been removed, had its name changed, or is temporarily unavailable.</p>
+            <Link to="/" replace className={`btn ${styles['btn']} ${styles['not-found-btn']}`}><i className="fa-solid fa-face-sad-cry"></i> Back to homepage</Link>
+        </div>
+    );
+} 
