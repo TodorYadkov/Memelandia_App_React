15674cf8b6f4cff2f5023a3ceecfdc91337f1c72
Todor Yadkov <toni_12@abv.bg>
2023-11-09 02:11:43 +0200

    Add route guard and new path to not-found 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 
 import { AuthProvider } from './components/core/contexts/AuthContext';
 
+import { RouteGuardPublic } from './components/core/guards/RouteGuardPublic';
+import { RoutGuardAuthenticated } from './components/core/guards/RouteGuardAuthenticated';
+
 import About from './components/features/about/About';
 import Footer from './components/features/footer/Footer';
 import Header from './components/features/header/Header';
@@ -13,6 +16,7 @@ import Profile from './components/features/users/profile/Profile';
 import Register from './components/features/users/register/Register';
 import ListUserMemes from './components/features/meme/list-user-memes/ListUserMemes';
 import Logout from './components/features/users/logout/Logout';
+import NotFound404 from './components/features/not-found-404/NotFound404';
 
 
 function App() {
@@ -25,21 +29,33 @@ function App() {
                     <Routes>
                         <Route path='/' element={<Home />} />
 
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/register' element={<Register />} />
-                        <Route path='/logout' element={<Logout />} />
-                        <Route path='/profile' element={<Profile />} />
+                        {/* Public routes */}
+                        <Route element={<RouteGuardPublic />}>
+                            <Route path='/login' element={<Login />} />
+                            <Route path='/register' element={<Register />} />
+                            <Route path='/about' element={<About />} />
+                        </Route>
 
+                        {/* Private routes */}
+                        <Route element={<RoutGuardAuthenticated />}>
+                            <Route path='/logout' element={<Logout />} />
+                            <Route path='/profile' element={<Profile />} />
+                        </Route>
+                        {/* Main meme route */}
                         <Route path='/memes/' element={<Outlet />}>
                             <Route path='catalog' element={<ListMemes />} />
                             <Route path='user-memes/:userId' element={<ListUserMemes />} />
                             <Route path='details/:memeId' element={<DetailsMeme />} />
-                            {/*<Route path='create' element={< />} />
-                            <Route path='edit/:memeId' element={< />} />
-                            <Route path='delete/:memeId' element={< />} /> */}
+
+                            {/* Private meme routes */}
+                            <Route element={<RoutGuardAuthenticated />}>
+                                {/*<Route path='create' element={< />} />
+                                <Route path='edit/:memeId' element={< />} />
+                                <Route path='delete/:memeId' element={< />} /> */}
+                            </Route>
                         </Route>
 
-                        <Route path='/about' element={<About />} />
+                        <Route path='*' element={<NotFound404 />} />
                     </Routes>
                 </div >
             </main >
