d3df75de4cea679c35b4b77516bc5e2ef81b4c3d
Todor Yadkov <toni_12@abv.bg>
2023-11-10 13:42:36 +0200

    Create new constant for pagination limit

--- client/src/components/core/environments/constants.jsx
+++ client/src/components/core/environments/constants.jsx
@@ -1,17 +1,18 @@
 const host = 'http://localhost:3000/';
+
 const endpoint = {
     register: 'users/register', // post
     login: 'users/login', // post
     logout: 'users/logout', // get
-    getUserBy: 'users/profile', // get
+    getUserById: 'users/profile', // get
     updateUser: 'users/profile', // put
     forgottenPass: 'users/forgotten-password', // put
     addRemoveFavoriteMeme: (memeId) => `users/favorite/${memeId}`, // get
     getTopRatedMemes: 'memes/three-top-rated', // get
-    getAllMemes: (page, limit) => `memes?page=${page}&limit=${limit}`, // get
+    getAllMemes: 'memes?', // get  -----> (page, limit) => `memes?page=${page}&limit=${limit}`
     getMemeBySearch: (memeName, category, page, limit) => `memes/search?name=${memeName}&category=${category}&page=${page}&limit=${limit}`, // get
     getMemeById: (memeId) => `memes/get-one/${memeId}`, // get
-    getMemeForUserById: (userId, page, limit) => `memes/gor-user/${userId}?page=${page}&limit=${limit}`, // get	
+    getMemeForUserById: (userId) => `memes/for-user/${userId}?`, // get  -----> (userId, page, limit) => `memes/for-user/${userId}?page=${page}&limit=${limit}`
     addNewMeme: 'memes/create', // post
     updateMeme: (memeId) => `memes/edit/${memeId}`, // put
     deleteMeme: (memeId) => `memes/delete/${memeId}`, // delete
@@ -26,8 +27,11 @@ const endpoint = {
 
 const tokenName = '43j54FD345HfiR3413fdErl326MCVzQ4543TRv23Ipo57';
 
+const paginationLimit = 1;
+
 export {
     host,
     endpoint,
-    tokenName
+    tokenName,
+    paginationLimit,
 };
