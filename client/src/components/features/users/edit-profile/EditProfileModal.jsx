94b07c5bee6adc66b4210940502011d35d56dc52
Todor Yadkov <toni_12@abv.bg>
2023-11-10 13:46:59 +0200

    Refactoring edit profile modal

--- client/src/components/features/users/edit-profile/EditProfileModal.jsx
+++ client/src/components/features/users/edit-profile/EditProfileModal.jsx
@@ -1,51 +1,198 @@
-export default function EditProfileModal() {
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import styles from './EditProfileModal.module.css';
+import { useApi } from '../../../core/hooks/useApi';
+import { useAuthContext } from '../../../core/hooks/useAuthContext';
+import { USER_FIELD } from '../userFieldConstants';
+import { trimInputData } from '../../../utils/trimInputData';
+import { endpoint } from '../../../core/environments/constants';
+import Message from '../../../shared/messages/Message';
+import Loading from '../../../shared/loader/Loading';
+
+/* eslint-disable react/prop-types */
+export default function EditProfileModal({ userDetails, setUserDetails, modalHandler }) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [serverMessage, setServerMessage] = useState({ error: '', success: '' }); // Use to display various messages from the server
+
+    const api = useApi();
+    const { addUserSession, getUserToken } = useAuthContext();
+
+    // Use react-hook-form https://react-hook-form.com/docs/useform/register
+    const { register, handleSubmit, formState: { errors, isValid, touchedFields, isSubmitting } } = useForm({
+        mode: 'onChange',
+        reValidateMode: 'onChange',
+        defaultValues: {
+            [USER_FIELD.username]: userDetails[USER_FIELD.username],
+            [USER_FIELD.email]: userDetails[USER_FIELD.email],
+            [USER_FIELD.name]: userDetails[USER_FIELD.name],
+            [USER_FIELD.age]: userDetails[USER_FIELD.age],
+        }
+    });
+
+    const submitHandler = (userData) => {
+        // Trim user input
+        const trimmedInput = trimInputData(userData);
+
+        setIsLoading(true);
+        api.put(endpoint.updateUser, trimmedInput)
+            .then(updatedData => {
+                setServerMessage({ success: 'Information changed successfully.' });
+                setUserDetails(updatedData);
+                addUserSession({
+                    accessToken: getUserToken,
+                    userDetails: {
+                        _id: updatedData._id,
+                        username: updatedData.username,
+                        email: updatedData.email,
+                        name: updatedData.name,
+                        age: updatedData.age,
+                        rating: updatedData.rating,
+                        createdUser: updatedData.createdAt,
+                        updatedUser: updatedData.updatedAt,
+                        commentsCount: updatedData.commentsCount
+                    }
+                });
+            })
+            .catch(error => setServerMessage({ error: error.message }))
+            .finally(() => setIsLoading(false));
+    };
+
     return (
         <div className="comment-modal">
-            <input type="checkbox" id="modal-toggle-edit-user-details" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-wrapper">
                     <div className="modal-header">
                         <h4><i className="fa-solid fa-user-pen"></i> Edit Profile</h4>
-                        <label htmlFor="modal-toggle-edit-user-details" className="modal-close"><i
-                            className="fa-solid fa-rectangle-xmark"></i></label>
+                        <button onClick={modalHandler} className="modal-close"><i className="fa-solid fa-rectangle-xmark"></i></button>
                     </div>
                     <div className="modal-content">
-                        {/* <!-- Use class valid and invalid when validate form --> */}
-                        TODO: Create direct with React
-                        {/* <!-- <form action="#" method="post" className="form">
-                            <div className="control-username">
-                                <label htmlFor="username" className="label username">Username</label>
-                                <input type="text" type="text" id="username" className="input valid username" placeholder="Username" />
+                        {(serverMessage?.error && !isLoading) && <Message type="error" message={serverMessage.error} />}
+                        {(serverMessage?.success && !isLoading) && <Message type="success" message={serverMessage.success} />}
+                        <form onSubmit={handleSubmit(submitHandler)} method="post" className={styles['form']}>
+                            {/* Username */}
+                            <div className={`${styles['control']}`}>
+                                <label htmlFor={USER_FIELD.username} className={`${styles['label']}`}>Username</label>
+                                <input
+                                    {...register(USER_FIELD.username, {
+                                        required: {
+                                            value: true,
+                                            message: 'Username is required'
+                                        },
+                                        minLength: {
+                                            value: 2,
+                                            message: 'Username must be at least two characters long'
+                                        },
+                                        maxLength: {
+                                            value: 15,
+                                            message: 'Username must not exceed fifteen characters'
+                                        }
+                                    })}
+                                    type="text"
+                                    id={USER_FIELD.username}
+                                    className={`${styles['input']} ${(touchedFields?.username && isValid) ? styles['valid'] : errors?.username ? styles['invalid'] : ''}`}
+                                    placeholder="Username"
+                                    required
+                                    minLength={2}
+                                    maxLength={15}
+                                />
+                                <i className="fa-solid fa-circle-user"></i>
+                                {errors?.username && (<p className={styles['error-message']}><span>{errors.username.message}</span></p>)}
                             </div>
-                            <div className="control-email">
-                                <label htmlFor="email" className="label email">Email</label>
-                                <input type="email" type="text" id="email" className="input invalid email" placeholder="Email" />
+                            {/* Email */}
+                            <div className={`${styles['control']}`}>
+                                <label htmlFor={USER_FIELD.email} className={`${styles['label']}`}>Email</label>
+                                <input
+                                    {...register(USER_FIELD.email, {
+                                        required: {
+                                            value: true,
+                                            message: 'Email is required'
+                                        },
+                                        pattern: {
+                                            value: /^[\w.-]+@[\w.-]+\.[\w]{2,}$/,
+                                            message: 'Invalid email address'
+                                        },
+                                    })}
+                                    type="email"
+                                    id={USER_FIELD.email}
+                                    className={`${styles['input']} ${(touchedFields?.email && isValid) ? styles['valid'] : errors?.email ? styles['invalid'] : ''}`}
+                                    placeholder="Email"
+                                    required
+                                />
+                                <i className="fa-solid fa-envelope"></i>
+                                {errors?.email && (<p className={styles['error-message']}><span>{errors.email.message}</span></p>)}
                             </div>
-                            <div className="control-name">
-                                <label htmlFor="name" className="label name">Name</label>
-                                <input type="name" type="text" id="name" className="input name" placeholder="Name" />
+                            {/* Name */}
+                            <div className={`${styles['control']}`}>
+                                <label htmlFor={USER_FIELD.name} className={`${styles['label']}`}>Name</label>
+                                <input
+                                    {...register(USER_FIELD.name, {
+                                        required: {
+                                            value: true,
+                                            message: 'Name is required'
+                                        },
+                                        minLength: {
+                                            value: 2,
+                                            message: 'Name must be at least two characters long'
+                                        },
+                                        maxLength: {
+                                            value: 30,
+                                            message: 'Name must not exceed thirty characters'
+                                        }
+                                    })}
+                                    type="name"
+                                    id={USER_FIELD.name}
+                                    className={`${styles['input']} ${(touchedFields?.name && isValid) ? styles['valid'] : errors?.name ? styles['invalid'] : ''}`}
+                                    placeholder="Name"
+                                    required
+                                    minLength={2}
+                                    maxLength={30}
+                                />
+                                <i className="fa-solid fa-user"></i>
+                                {errors?.name && (<p className={styles['error-message']}><span>{errors.name.message}</span></p>)}
                             </div>
-                            <div className="control-age">
-                                <label htmlFor="age" className="label age">Age</label>
-                                <input type="age" type="text" id="age" className="input age" placeholder="Age" />
+                            {/* Age */}
+                            <div className={`${styles['control']} ${styles['age']}`}>
+                                <label htmlFor={USER_FIELD.age} className={`${styles['label']}`}>Age</label>
+                                <input
+                                    {...register(USER_FIELD.age, {
+                                        valueAsNumber: true,
+                                        required: {
+                                            value: true,
+                                            message: 'Age is required'
+                                        },
+                                        min: {
+                                            value: 12,
+                                            message: 'Age must be at least 12 years old'
+                                        },
+                                        max: {
+                                            value: 120,
+                                            message: 'Age must not exceed 120 years old'
+                                        }
+                                    })}
+                                    type="number"
+                                    id={USER_FIELD.age}
+                                    className={`${styles['input']} ${(touchedFields?.age && isValid) ? styles['valid'] : errors?.age ? styles['invalid'] : ''}`}
+                                    placeholder="Age"
+                                    step={1}
+                                    required
+                                    min={12}
+                                    max={120}
+                                />
+                                <i className="fa-solid fa-id-card"></i>
+                                {errors?.age && (<p className={styles['error-message']}><span>{errors.age.message}</span></p>)}
                             </div>
-                            <div className="control-password">
-                                <label htmlFor="password" className="label password">Password</label>
-                                <input type="password" type="text" id="password" className="input password" placeholder="Password" />
+                            {/* Buttons */}
+                            <div className={styles['form-buttons']}>
+                                {isLoading
+                                    ? <Loading width="50px" height="50px" />
+                                    : <button disabled={!isValid || isSubmitting || (serverMessage?.success && !isLoading)} className={`btn ${styles['btn']} ${styles['btn-edit-profile']}`}><i className="fa-solid fa-user-pen"></i> Edit</button>
+                                }
                             </div>
-                            <div className="control-rePass">
-                                <label htmlFor="rePass" className="label rePass">Confirm Password</label>
-                                <input type="password" type="text" id="rePass" className="input rePass" placeholder="Confirm Password" />
-                            </div>
-                            <div className="form-buttons">
-                                <a href="#" className="btn btn-register">REGISTER</a>
-                                <a href="#" className="btn btn-link">I already have an account</a>
-                            </div>
-                        </form> --> */}
+                        </form>
                     </div>
                     <div className="modal-footer">
-                        <label htmlFor="modal-toggle-edit-user-details" className="modal-close"><i className="fa-solid fa-xmark"></i>
-                            Close</label>
+                        <button onClick={modalHandler} className="modal-close"><i className="fa-solid fa-xmark"></i> Close</button>
                     </div>
                 </div>
             </div>
