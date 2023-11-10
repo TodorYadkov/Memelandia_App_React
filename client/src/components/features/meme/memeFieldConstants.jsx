fe0e081b3fc0740f66d1c2a96d943ad1db5a087b
Todor Yadkov <toni_12@abv.bg>
2023-11-10 13:44:17 +0200

    Create constants for memes

---
+++ client/src/components/features/meme/memeFieldConstants.jsx
@@ -0,0 +1,13 @@
+export const MEME_FIELD = {
+    name: 'name',
+    imageUrl: 'imageUrl',
+    category: 'category',
+};
+
+// Category for memes
+export const category = ['Funny', 'Sports', 'Politics', 'Science',
+    'Technology', 'Food', 'Travel', 'Music', 'Movies',
+    'Gaming', 'Literature', 'Nature', 'Art', 'History',
+    'Fashion', 'Fitness', 'Animals', 'Celebrities',
+    'Hobbies', 'Educational', 'Inspirational',
+    'Relationships', 'Humor', 'Other'];
