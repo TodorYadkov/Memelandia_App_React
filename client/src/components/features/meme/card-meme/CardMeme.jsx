9cebb217634531708effa8f58b2606f843baca40
Todor Yadkov <toni_12@abv.bg>
2023-11-11 10:12:50 +0200

    Refactoring - not ready yet

--- client/src/components/features/meme/card-meme/CardMeme.jsx
+++ client/src/components/features/meme/card-meme/CardMeme.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './CardMeme.module.css';
 import { useAuthContext } from '../../../core/hooks/useAuthContext';
+import { useApi } from '../../../core/hooks/useApi';
+import { endpoint } from '../../../core/environments/constants';
 
 import formatDateToTimeAgo from '../../../utils/formatDateToTimeAgo';
 import Rating from '../../rating/Rating';
 import AddCommentModal from '../../comment/add-comment/AddCommentModal';
 import DeleteMemeModal from '../delete-meme/DeleteMemeModal';
+import Loading from '../../../shared/loader/Loading';
+import Message from '../../../shared/messages/Message';
 
 
 export default function CardMeme({
@@ -24,65 +28,94 @@ export default function CardMeme({
     createdAt,
     updatedAt,
 }) {
-    const { isLoggedIn, getUserDetails } = useAuthContext();
+    const [userDetails, setUserDetails] = useState({});                             // Use to show user details
+    const [isLoading, setIsLoading] = useState(false);
+    const [serverMessage, setServerMessage] = useState({ error: '' });               // Use to display various messages from the server
 
-    if (isLoggedIn) {
-        const currentUser = {
-            isLogged: isLoggedIn,
-            // eslint-disable-next-line eqeqeq
-            isOwner: getUserDetails?._id == author?._id,
-        };
+    const api = useApi();
+    const { isLoggedIn, getUserDetails, addUserSession, getUserToken } = useAuthContext();
 
+    useEffect(() => {
 
-        return (
-            <article className={styles['card']}>
-                <header className={styles['card-header']}>
+        // If the user has refreshed their browser, get their data from the server
+        if (getUserDetails['_id'] && isLoggedIn) {
+            setUserDetails(getUserDetails); // Get user details from context (in memory)
 
-                    <h4><Link to={`/memes/details/${_id}`}>{name}</Link></h4>
+        } else if (isLoggedIn) {
+            setIsLoading(true);
+            api.get(endpoint.getUserById)
+                .then(userData => {
+                    setUserDetails(userData);
+                    // Store user data for optimized next request, don't get details from server again
+                    addUserSession({
+                        accessToken: getUserToken,
+                        userDetails: { ...userData }
+                    });
+                })
+                .catch(error => setServerMessage({ error: error.message }))
+                .finally(() => setIsLoading(false));
+        }
 
-                    <p className={styles['card-author-name']}>By: <Link to={`/memes/user-memes/${author._id}`}>{author.username}</Link>, <time>{formatDateToTimeAgo(createdAt)}</time> ago</p>
-                    <div className={styles['card-meme-info']}>
-                        <p className={styles['info']}>Category: <Link to={`/memes/catalog?category=${category}`}>{category}</Link></p>
-                        <p className={styles['info']}><i className="fa-regular fa-eye"></i> {views}</p>
-                        <p className={styles['info']}><i className="fa-regular fa-thumbs-up"></i> {likes.length}</p>
-                        <p className={styles['info']}><i className="fa-regular fa-thumbs-down"></i> {dislikes.length}</p>
+    }, []);
 
-                        <Rating rating={rating} />
-                    </div>
-                </header>
-                <div className={styles['card-img-wrapper']}>
+    const currentUser = {
+        isLogged: isLoggedIn,
+        isOwner: userDetails?._id === author?._id,
+    };
 
-                    <Link to={`/memes/details/${_id}`}><img src={imageUrl} alt={name} /></Link>
+    return (
+        <article className={styles['card']}>
+
+            {isLoading && <Loading />}
+            {(serverMessage?.error && !isLoading) && <Message type="error" message={serverMessage.error} />}
+
+            <header className={styles['card-header']}>
+
+                <h4><Link to={`/memes/details/${_id}`}>{name}</Link></h4>
+
+                <p className={styles['card-author-name']}>By: <Link to={`/memes/user-memes/${author._id}`} className={styles['card-author-link']}>{author.username}</Link>, <time>{formatDateToTimeAgo(createdAt)}</time> ago</p>
+
+                <div className={styles['card-meme-info']}>
+                    <p className={styles['category']}>Category: <Link to={`/memes/catalog?category=${category}`}>{category}</Link></p>
+                    <p className={styles['info']}><i className="fa-regular fa-eye"></i> {views}</p>
+                    <p className={styles['info']}><i className="fa-regular fa-thumbs-up"></i> {likes.length}</p>
+                    <p className={styles['info']}><i className="fa-regular fa-thumbs-down"></i> {dislikes.length}</p>
+
+                    <Rating rating={rating} />
                 </div>
+            </header>
+            <div className={styles['card-img-wrapper']}>
 
-                {currentUser?.isLoggedIn
-                    ?
-                    <footer className={styles['card-footer']}>
-                        {currentUser?.isOwner
-                            ?
-                            <>
-                                <p><a className={`btn ${styles['btn']} ${styles['edit']}`} href="#"><i className="fa-solid fa-pen-to-square"></i></a></p>
-                                <label htmlFor="modal-toggle-meme-delete" className={`btn ${styles['btn']} ${styles['delete']} ${styles['modal-button']}`}><i className="fa-solid fa-trash"></i></label>
-                            </>
-                            :
-                            <>
-                                <p><a className={`btn ${styles['btn']} ${styles['like']}`} href="#"><i className="fa-solid fa-thumbs-up"></i></a></p>
-                                <p><a className={`btn ${styles['btn']} ${styles['dislike']}`} href="#"><i className="fa-solid fa-thumbs-down"></i></a></p>
-                                <p><a className={`btn ${styles['btn']} ${styles['comment']}`} href="#"><i className="fa-solid fa-message"></i></a></p>
-                                <p><a className={`btn ${styles['btn']} ${styles['favorite']}`} href="#"><i className="fa-solid fa-heart"></i></a></p>
-                            </>
-                        }
+                <Link to={`/memes/details/${_id}`}><img src={imageUrl} alt={name} /></Link>
+            </div>
 
-                        <label htmlFor="modal-toggle-share" className={`btn ${styles['btn']} ${styles['share']} ${styles['modal-button']}`}><i
-                            className="fa-solid fa-share-nodes"></i></label>
-                    </footer>
+            {currentUser?.isLogged
+                ?
+                <footer className={styles['card-footer']}>
+                    {currentUser?.isOwner
+                        ?
+                        <>
+                            <p><a className={`btn ${styles['edit']}`} href="#"><i className="fa-solid fa-pen-to-square"></i></a></p>
+                            <label htmlFor="modal-toggle-meme-delete" className={`btn ${styles['delete']} ${styles['modal-button']}`}><i className="fa-solid fa-trash"></i></label>
+                        </>
+                        :
+                        <>
+                            <p><a className={`btn ${styles['like']}`} href="#"><i className="fa-solid fa-thumbs-up"></i></a></p>
+                            <p><a className={`btn ${styles['dislike']}`} href="#"><i className="fa-solid fa-thumbs-down"></i></a></p>
+                            <p><a className={`btn ${styles['comment']}`} href="#"><i className="fa-solid fa-message"></i></a></p>
+                            <p><a className={`btn ${styles['favorite']}`} href="#"><i className="fa-solid fa-heart"></i></a></p>
+                        </>
+                    }
 
-                    : null
-                }
+                    <label htmlFor="modal-toggle-share" className={`btn ${styles['share']} ${styles['modal-button']}`}><i
+                        className="fa-solid fa-share-nodes"></i></label>
+                </footer>
 
-            </article>
-        );
-    }
+                : null
+            }
+
+        </article>
+    );
 }
 
 // <DeleteMemeModal />
