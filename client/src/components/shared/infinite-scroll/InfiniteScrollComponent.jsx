cdbc9aa176f01f59d1db53309d7cacfbe2f5aeb4
Todor Yadkov <toni_12@abv.bg>
2023-11-13 10:43:05 +0200

    Refactoring and fix the problem whe the user is search the state is not persist

--- client/src/components/shared/infinite-scroll/InfiniteScrollComponent.jsx
+++ client/src/components/shared/infinite-scroll/InfiniteScrollComponent.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import styles from './InfiniteScrollComponent.module.css';
 import { paginationLimit } from '../../core/environments/constants';
@@ -10,72 +11,77 @@ import Loading from '../loader/Loading';
 import Message from '../messages/Message';
 import NoContentMessage from '../no-content/NoContentMessage';
 
-export const InfiniteScrollComponent = ({ endpoint, setFetchedMemes }) => {
-    const [memes, setMemes] = useState([]);
+export const InfiniteScrollComponent = ({ endpoint, setFetchedMeme }) => {
+    const [memes, setMemes] = useState([]);                                                             // State of all memes to render
     const [isLoading, setIsLoading] = useState(false);
     const [serverMessage, setServerMessage] = useState({ error: '' });
-    const [currentPage, setCurrentPage] = useState(1);                          // Current page
-    const [totalPages, setTotalPages] = useState(1);                            // Total number of pages from the server
+    const [currentPage, setCurrentPage] = useState(1);                                                  // Current page to fetch data from the server
+    const [totalPages, setTotalPages] = useState(1);                                                    // Total number of pages from the server
+
+    const [searchParams] = useSearchParams();                                                           // Use to handle query params
 
     const api = useApi();
 
     useEffect(() => {
-        // Add a scroll event listener
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll);                                                // Add a scroll event listener
 
-        // Cleanup the event listener when this is the last page
-        if (totalPages <= currentPage) {
+        if (totalPages <= currentPage) {                                                                // Cleanup the event listener when this is the last page
             window.removeEventListener('scroll', handleScroll);
         }
 
-        // Get memes from the server
         setIsLoading(true);
-        api.get(`${endpoint}page=${currentPage}&limit=${paginationLimit}`)
+        api.get(`${endpoint}page=${currentPage}&limit=${paginationLimit}`)                              // Get memes from the server
             .then(serverData => {
-                // Check for data, if not set an empty array and reset page count
-                if (serverData.totalPages === 0) {
+                if (serverData.totalPages === 0) {                                                      // Check for data, if not set an empty array and reset page count
                     setMemes([]);
                     setCurrentPage(1);
 
-                } else {
-                    // If has data get only unique memes
+                } else {                                                                                // If has data get only unique memes
                     setMemes((stateMemes) => {
                         const uniqueId = new Set(stateMemes.map((meme) => meme._id));                   // To make sure I only show unique memes, I keep the old ID state
                         const uniqueMemes = serverData.memes.filter((meme) => !uniqueId.has(meme._id)); // If the old ID is in the current server response, I don't add it to the current state
                         return [...stateMemes, ...uniqueMemes];                                         // Return only unique memes
                     });
                 }
-                // Get total number of pages from the server
-                setTotalPages(serverData.totalPages);
 
-                // This is utilized on the user's memes page to retrieve user details without making any additional server requests.
-                ((setFetchedMemes && currentPage === 1) && (serverData.memes.length > 0)) && (
-                    setFetchedMemes({ ...serverData.memes[0].author, userMemesCount: serverData.userMemesCount })
-                );
+                setTotalPages(serverData.totalPages);                                                   // Get total number of pages from the server
+
+                ((setFetchedMeme && currentPage === 1) && (serverData.memes.length > 0))                // This is utilized on the user's memes page to retrieve user details without making any additional server requests.
+                    && (setFetchedMeme({ ...serverData.memes[0].author, userMemesCount: serverData.userMemesCount }));
 
             })
             .catch(error => setServerMessage({ error: error.message }))
             .finally(() => setIsLoading(false));
 
-        // Cleanup the event listener when the component unmounts
-        return () => {
+        return () => {                                                                                  // Cleanup the event listener when the component unmounts
             window.removeEventListener('scroll', handleScroll);
         };
 
     }, [currentPage, totalPages, endpoint]);
 
+
+    useEffect(() => {                                                                                   // Use to set a new empty state to memes when the search params change
+        if (searchParams) {                                                                             // Reset state and re-fetch when the search parameters change
+            setMemes([]);
+            setCurrentPage(1);
+        }
+    }, [searchParams]);
+
     const handleScroll = async () => {
-        // Define the threshold value before page end and where to trigger the next page
-        const threshold = 250;
+
+        const threshold = 250;                                                                          // Define the threshold value before page end and where to trigger the next page
 
         // Check if the user has scrolled close to the bottom of the page
         // https://developer.mozilla.org/en-US/docs/Web/API/Window/innerHeight
         // https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement
         // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop
         // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetHeight
-        if (window.innerHeight + document.documentElement.scrollTop + threshold >= document.documentElement.offsetHeight) {
+        if (window.innerHeight
+            + document.documentElement.scrollTop
+            + threshold
+            >= document.documentElement.offsetHeight) {
 
-            setCurrentPage(prevPage => prevPage + 1); // Add next page
+            setCurrentPage(prevPage => prevPage + 1);                                                   // Add next page
         }
     };
 
