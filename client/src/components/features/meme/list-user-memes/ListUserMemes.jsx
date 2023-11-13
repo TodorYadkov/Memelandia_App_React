0bf76e78e9ba907859662c896d03a63ff599d4ef
Todor Yadkov <toni_12@abv.bg>
2023-11-13 10:42:11 +0200

    Add new global css class and when the page is loaded to scroll on the top the page

--- client/src/components/features/meme/list-user-memes/ListUserMemes.jsx
+++ client/src/components/features/meme/list-user-memes/ListUserMemes.jsx
@@ -4,41 +4,48 @@ import { useParams } from 'react-router-dom';
 import styles from './ListUserMemes.module.css';
 import { endpoint } from '../../../core/environments/constants';
 import { InfiniteScrollComponent } from '../../../shared/infinite-scroll/InfiniteScrollComponent';
+import { scrollToTop } from '../../../utils/scrollToTop';
+
+import Rating from '../../rating/Rating';
 
 export default function ListUserMemes() {
-    const [currenEndPointForUserMemes, setCurrenEndPointForUserMemes] = useState('');   // Use to save an endpoint for the current user
-    const [fetchedMemes, setFetchedMemes] = useState([]);                               // Use to get details about the current user from the fetched memes
-    const [userDetails, setUserDetails] = useState({});                                 // Use to display details about the current user
+    const [currenEndPointForUserMemes, setCurrenEndPointForUserMemes] = useState('');                   // Use to save an endpoint for the current user
+    const [fetchedMeme, setFetchedMeme] = useState([]);                                                 // Use to get details about the current user from the fetched memes
+    const [userDetails, setUserDetails] = useState({});                                                 // Use to display details about the current user
 
-    const { userId } = useParams('userId');                                             // Get userId from query params
+    const { userId } = useParams('userId');                                                             // Get userId from query params
 
     useEffect(() => {
-        // Add page title
-        document.title = `${userDetails?.username ? `${userDetails.username}'s` : 'User'} memes`;
+        document.title = `${userDetails?.username ? `${userDetails.username}'s` : 'User'} memes`;       // Add page title
+        scrollToTop();                                                                                  // Scroll to the top of the page
 
-        // Set end point to get all the memes for the user
-        setCurrenEndPointForUserMemes(endpoint.getMemeForUserById(userId));
+        setCurrenEndPointForUserMemes(endpoint.getMemeForUserById(userId));                             // Set endpoint to get all the memes for the user
 
-        setUserDetails(fetchedMemes);
+        setUserDetails(fetchedMeme);                                                                    // Set user details from InfiniteScrollComponent
 
-    }, [fetchedMemes]);
+    }, [fetchedMeme]);
 
     return (
-        <>
+        <div className='max-width'>
             <div className={styles['user-card']}>
                 <div className={styles['user-card-header']}>
-                    <h1>{`Explore ${userDetails?.username}'s Collection`}</h1>
+                    <h1>Explore <span>{userDetails?.username}</span>&apos;s Collection</h1>
                 </div>
                 <div className={styles['user-card-content']}>
-                    <p><i className="fa-regular fa-star"></i> Rating: {userDetails?.rating} points</p>
-                    <p><i className="fa-regular fa-images"></i> Number of Memes: {userDetails?.userMemesCount}</p>
+                    <div className={styles['user-rating']}>
+                        <Rating rating={userDetails?.rating} />
+                        <p>Rating: <span>{userDetails?.rating}</span> points</p>
+                    </div>
+                    <p>
+                        <i className="fa-regular fa-images"></i> Number of Memes: <span>{userDetails?.userMemesCount}</span>
+                    </p>
                 </div>
                 <div className={styles['user-card-footer']}>
-                    <p>Joined: {userDetails?.createdAt}</p>
+                    <p>Joined: {new Date(userDetails?.createdAt).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
                 </div>
             </div>
 
-            {currenEndPointForUserMemes && <InfiniteScrollComponent endpoint={currenEndPointForUserMemes} setFetchedMemes={setFetchedMemes} />}
-        </>
+            {currenEndPointForUserMemes && <InfiniteScrollComponent endpoint={currenEndPointForUserMemes} setFetchedMeme={setFetchedMeme} />}
+        </div>
     );
 }
