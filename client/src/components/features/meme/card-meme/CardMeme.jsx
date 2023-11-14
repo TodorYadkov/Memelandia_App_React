126e0ce23ad7e50b13ce6f059fb62a39a71a2118
Todor Yadkov <toni_12@abv.bg>
2023-11-14 02:23:10 +0200

    Fix problem with add new comment from different places with adding optional handler

--- client/src/components/features/meme/card-meme/CardMeme.jsx
+++ client/src/components/features/meme/card-meme/CardMeme.jsx
@@ -7,13 +7,14 @@ import { useAuthContext } from '../../../core/hooks/useAuthContext';
 import { useApi } from '../../../core/hooks/useApi';
 import { endpoint } from '../../../core/environments/constants';
 import formatDateToTimeAgo from '../../../utils/formatDateToTimeAgo';
+import { useModal } from '../../../core/hooks/useModal';
 
 import Rating from '../../rating/Rating';
 import AddCommentModal from '../../comment/add-comment/AddCommentModal';
 import DeleteMemeModal from '../delete-meme/DeleteMemeModal';
 import Loading from '../../../shared/loader/Loading';
 import Message from '../../../shared/messages/Message';
-
+import LikeMeme from '../../like/LikeMeme';
 
 export default function CardMeme({
     _id,
@@ -27,13 +28,19 @@ export default function CardMeme({
     views,
     createdAt,
     updatedAt,
+    setNewCommentHandler,                                                                               // Optional handler, when adding a new comment, updates list of all comments (state)
 }) {
     const [userDetails, setUserDetails] = useState({});                                                 // Use to show user details
     const [isLoading, setIsLoading] = useState(false);
     const [serverMessage, setServerMessage] = useState({ error: '' });                                  // Use to display various messages from the server
+    const [likeState, setLikeState] = useState({                                                        // Use to keep a state of likes
+        likeArr: likes,                                                                                 // Use to add or remove from the state of a user after a request to the server
+        isUserAlreadyLiked: false                                                                       // Initial state of the user
+    });
 
     const api = useApi();
     const { isLoggedIn, getUserDetails, addUserSession, getUserToken } = useAuthContext();
+    const [isShownAddCommentModal, setIsShownAddCommentModal] = useModal();                             // Show hide modal for add comment
 
     useEffect(() => {
         if (getUserDetails['_id'] && isLoggedIn) {                                                      // If the user has refreshed their browser, get their data from the server
@@ -53,7 +60,9 @@ export default function CardMeme({
                 .finally(() => setIsLoading(false));
         }
 
-    }, []);
+        setLikeState(state => ({ ...state, isUserAlreadyLiked: likes.includes(userDetails._id) }));     // Check for the status of the current user, whether he has liked the current card
+
+    }, [userDetails]);
 
     const currentUser = {                                                                               // Get state of the current user
         isLogged: isLoggedIn,
@@ -76,14 +85,14 @@ export default function CardMeme({
                         <i className="fa-solid fa-at"></i> {author.username}
                     </Link>
                     <span>,</span>
-                    <time>{formatDateToTimeAgo(createdAt)}</time>
-                    <span>ago</span>
+                    <time>{createdAt !== updatedAt ? formatDateToTimeAgo(updatedAt) : formatDateToTimeAgo(createdAt)}</time>
+                    {createdAt !== updatedAt ? <sup>(edited)</sup> : <span>ago</span>}
                 </p>
 
                 <div className={styles['card-meme-info']}>
                     <p className={styles['category']}>Category: <Link to={`/memes/catalog?category=${category}`}>{category}</Link></p>
                     <p className={styles['info']}><i className="fa-regular fa-eye"></i> {views}</p>
-                    <p className={styles['info']}><i className="fa-regular fa-thumbs-up"></i> {likes.length}</p>
+                    <p className={styles['info']}><i className="fa-regular fa-thumbs-up"></i> {likeState.likeArr.length}</p>
                     <p className={styles['info']}><i className="fa-regular fa-thumbs-down"></i> {dislikes.length}</p>
 
                     <Rating rating={rating} />
@@ -105,9 +114,9 @@ export default function CardMeme({
                         </>
                         :
                         <>
-                            <p><a className={`btn ${styles['like']}`} href="#"><i className="fa-solid fa-thumbs-up"></i></a></p>
+                            <LikeMeme memeId={_id} likeState={likeState} setLikeState={setLikeState} />
                             <p><a className={`btn ${styles['dislike']}`} href="#"><i className="fa-solid fa-thumbs-down"></i></a></p>
-                            <p><a className={`btn ${styles['comment']}`} href="#"><i className="fa-solid fa-message"></i></a></p>
+                            <p><button onClick={setIsShownAddCommentModal} className={styles['btn-add-comment']}><i className="btn fa-solid fa-message"></i></button></p>
                             <p><a className={`btn ${styles['favorite']}`} href="#"><i className="fa-solid fa-heart"></i></a></p>
                         </>
                     }
@@ -119,12 +128,12 @@ export default function CardMeme({
                 : null
             }
 
+            {isShownAddCommentModal &&
+                <AddCommentModal
+                    memeId={_id}
+                    modalHandler={setIsShownAddCommentModal}
+                    {...(setNewCommentHandler && { setNewCommentHandler })} />
+            }
         </article>
     );
 }
-
-
-// const [isShownAddCommentModal, setIsShownAddCommentModal] = useModal();                 // Show hide modal for add comment
-// {isShownAddCommentModal && <AddCommentModal modalHandler={setIsShownAddCommentModal} memeId={memeInfo._id} />}
-// <DeleteMemeModal />
-// <AddCommentModal />
