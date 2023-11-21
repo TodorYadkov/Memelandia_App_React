4d0ad8698ce6dd221eb323d81812993b0a2f1264
Todor Yadkov <toni_12@abv.bg>
2023-11-21 02:05:34 +0200

    Add max limit on image

--- server/config/expressConfig.js
+++ server/config/expressConfig.js
@@ -3,13 +3,13 @@ const addCORS = require('../middlewares/addCORS');
 const userSession = require('../middlewares/userSession');
 
 module.exports = (app) => {
-    
+
     // Add CORS
     app.use(addCORS());
     // Add middleware to use JSON
-    app.use(express.json());
+    app.use(express.json({ limit: '10mb' }));
     // Add middleware to get data from body and from query
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.urlencoded({ extended: false, limit: '10mb' }));
     // Check user token
     app.use(userSession());
 
