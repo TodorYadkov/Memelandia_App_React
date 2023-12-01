0946beca1ba8f4df045ba6d4c3dfbaffac7c7e53
Todor Yadkov <toni_12@abv.bg>
2023-12-01 16:55:10 +0200

    Add vitest to project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,7 @@
+// https://github.com/vitest-dev/vitest/tree/main/examples/react-testing-lib
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
@@ -5,4 +9,10 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: ['./jest-setup.js'],
+  },
 });
